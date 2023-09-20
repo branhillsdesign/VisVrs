21765540a847e465daaf5d1a7aaff058a19c6d21
Brandon Hills <brandon@branhills.com>
2023-09-20 11:32:50 -0500

    update

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,22 +2,22 @@ import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
 export async function get() {
-	let posts = await getCollection('posts')
+  let posts = await getCollection('posts')
 
-	posts.sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate))
+  posts.sort((a, b) => new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime());
 
-	posts = posts.slice(0, 3)
+  posts = posts.slice(0, 3)
 
-	return rss({
-		title: 'VisVrs: An Astro Theme for Visual Storytelling',
-		description: 'VisVrs is an Astro theme specifically designed for photo and text blogging, making it ideal for blog authors and content creators.',
-		site: 'https://visvrs.vercel.app',
-		items: posts.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.pubDate,
-			description: post.data.description,
-			link: `${post.slug}/`
-		})),
-		customData: `<language>en</language>`
-	})
+  return rss({
+    title: 'BranHills.com - Blog',
+    description: 'Personal website and portfolio of Brandon Hills, Staff Product Designer at BizLibrary.com',
+    site: 'https://www.branhills.com/',
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `${post.slug}/`
+    })),
+    customData: `<language>en</language>`
+  })
 }
